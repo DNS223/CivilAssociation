77f1a98307f7741a36c5a33f9bdd95681c442127
Daniel <npm030312@gmail.com>
2024-10-31 10:42:30 -0600

    Envio de mensaje a correo

--- src/contactUs/ContactUs.jsx
+++ src/contactUs/ContactUs.jsx
@@ -1,5 +1,25 @@
+import { useRef } from 'react';
 import './ContactUsStyle.css'
+import emailjs from '@emailjs/browser';
 export default function ContactUs(){
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    alert("Se envia")
+    emailjs
+      .sendForm('service_fxzhw5f', 'template_xn0lv9i', form.current, {
+        publicKey: 'fZ722ARiU_SLVQjMg',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
     return(
         <div className="ContactSectionContainer">
             <div className='ContactContainer'>
@@ -9,18 +29,18 @@ export default function ContactUs(){
 
                 <div className='ContactRightContainer'>
                     <img src="./LogoParrf.jpeg" alt="" className='logoForm'/>
-                    <div className='FormContainer'>
+                    <form className='FormContainer' ref={form} onSubmit={sendEmail}>
                         <div className='FirstsInputs'>
-                            <input type="text" className='firstInput inputContact' placeholder='Nombre'/>
-                            <input type="text" className='firstInput inputContact' placeholder='Apellido'/>
+                            <input type="text" className='firstInput inputContact' placeholder='Nombre' name="user_name"/>
+                            <input type="text" className='firstInput inputContact' placeholder='Apellido' name="user_lastname"/>
                         </div>
                         <div className='OtherInputs'>
-                        <input type="text" className='ohterInputs inputContact' placeholder='Teléfono'/>
-                        <input type="text" className='ohterInputs inputContact' placeholder='Correo electrónico'/>
-                        <input type="text" className='ohterInputs inputContact' placeholder='Motivo de donación'/>
-                        <input type="button" className='ohterInputs inputContact btnEnviar' placeholder='Teléfono' value="Enviar"/>
+                        <input type="text" className='ohterInputs inputContact' placeholder='Teléfono' name="user_phone"/>
+                        <input type="email" className='ohterInputs inputContact' placeholder='Correo electrónico' name="user_email"/>
+                        <input type="text" className='ohterInputs inputContact' placeholder='Motivo de donación' name="message"/>
+                        <input type="submit" className='ohterInputs inputContact btnEnviar'  value="Enviar"/>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
