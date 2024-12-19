81cc60a641e286512a15ac29d979cfbc8d5ef54b
Daniel <npm030312@gmail.com>
2024-12-18 21:20:26 -0600

    Enlace a sección con animación

--- src/componentes/CardServicio/CardServicio.jsx
+++ src/componentes/CardServicio/CardServicio.jsx
@@ -1,9 +1,20 @@
 import "./CardServicio.css"
+import { Link, animateScroll as scroll } from "react-scroll";
 export default function CardServicio({image, titulo, parrafo, link}){
     return(
         <div className="CardServicioContainer" >
             <img src={image} alt="" className="iconCard"/>
-            <a className="titulo" href={link}>{titulo}</a>
+            {link=="/AboutUs" ? <a className="titulo" href={link}>{titulo}</a>: 
+            <Link
+            className="titulo"
+            to={link.replace("#", "")} // Quita el "#" para usar el ID directamente
+            smooth={true}
+            duration={500}
+            offset={-50} // Ajusta si tienes un navbar fijo
+          >
+            {titulo}
+          </Link>}
+            
             <p className="parrafo">{parrafo}</p>
         </div>
     )
